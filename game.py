bd3445bfc13b12fa43f752f12c5f3e143be14fbf
shayaan <shayaan.hussain2@gmail.com>
2021-02-06 17:05:55 +0530

    shayaan

--- game.py
+++ game.py
@@ -68,7 +68,7 @@ def checkboat(boatX, boatY, playerX, playerY):
         if boatX - playerX < 20 and boatX - playerX > -45:
             return True
         else:
-            print("hello")
+            print("hey")
             return False
     else:
         return False
